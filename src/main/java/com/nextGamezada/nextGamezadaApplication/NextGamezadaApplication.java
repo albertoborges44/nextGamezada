c66ff4423ca561446eecd20af4f0d8acc8459f59
Alberto Borges <albertoborges44@gmail.com>
2024-02-04 23:02:48 -0400

    feat: add initial project structure

---
+++ src/main/java/com/nextGamezada/nextGamezadaApplication/NextGamezadaApplication.java
@@ -0,0 +1,13 @@
+package com.nextGamezada.nextGamezadaApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextGamezadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextGamezadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nextGamezada/nextGamezadaApplication/NextGamezadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextGamezada.nextGamezadaApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextGamezadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
